c36d44e5531d319b48576e4108cb5aed17769a0b
Grant Hill <granthill@live.com>
2022-08-21 11:33:01 -0400

    first commit.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IterationExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Iterations exercises program.");
+            Console.WriteLine("");
+            Console.WriteLine("To start, we will have a variable assigned to a value, have a condition determined for it, and then have it incremented until it surpasses the condition.");
+            Console.WriteLine("");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
